c216e35e16d3ffb685113f9c6a45228bb233bb62
MubashirUsman <pink55651@gmail.com>
2024-03-31 23:50:15 +0200

    lookup time in ms

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI, HTTPException, Query
 import time
-import dns.resolver
+from dns import resolver
 
 app = FastAPI()
 
 @app.get("/dns-lookup")
 def perform_dns_lookup(domain: str = Query(..., min_length=1), server: str = Query(..., min_length=1)):
     response_time = dns_lookup(domain, server)
-    return {"domain": domain, "server": server, "response_time": response_time}
+    return {"domain": domain, "server": server, "response_time": 1000*response_time}
 
 
 def dns_lookup(domain, server):
-    resolver = dns.resolver.Resolver()
-    resolver.nameservers = [server]
+    resolver_instance = resolver.Resolver()
+    resolver_instance.nameservers = [server]
 
     start_time = time.time()
     try:
